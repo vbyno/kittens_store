dfe9e01254aa8f0ab6ea8f52a93051e2cee42c77
vbyno <volodymyr.byno@gmail.com>
2021-08-08 10:27:50 +0300

    Proper modules separation: global for VPC, live for EC2

--- ops/infrastructure/live/outputs.tf
+++ ops/infrastructure/live/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
-  value = data.terraform_remote_state.vpc.outputs.vpc_id
+  value = data.terraform_remote_state.vpc_state.outputs.vpc_id
+}
+
+output "public_ip" {
+  value = module.aws_ec2.public_ip
+}
+
+output "security_group_id" {
+  value = module.aws_ec2.security_group_id
 }
