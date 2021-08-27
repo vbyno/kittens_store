ddfe8c9fa70dfaf4d4a2c9e4aeee7f4b2ec58e03
vbyno <volodymyr.byno@gmail.com>
2021-08-27 11:21:49 +0300

    Temp (ecr)

--- ops/infrastructure/global/output.tf
+++ ops/infrastructure/global/output.tf
@@ -1,11 +1,11 @@
-output "vpc_id" {
-  value = module.aws_vpc.id
-}
+# output "vpc_id" {
+#   value = module.aws_vpc.id
+# }
 
-output "subnet_ids" {
-  value = module.aws_vpc.subnet_ids
-}
+# output "subnet_ids" {
+#   value = module.aws_vpc.subnet_ids
+# }
 
-output "vpc" {
-  value = module.aws_vpc.vpc
-}
+# output "vpc" {
+#   value = module.aws_vpc.vpc
+# }
