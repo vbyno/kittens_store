236d2bcd8ea9a7cc7c873661894afd1b38e9fda0
vbyno <volodymyr.byno@gmail.com>
2021-09-19 09:50:05 +0300

    Main Github Workflow: terraform apply, docker build, helm upgrade

--- ops/infrastructure/modules/ecr/main.tf
+++ ops/infrastructure/modules/ecr/main.tf
@@ -6,3 +6,51 @@ resource "aws_ecr_repository" "ecr_repo" {
     scan_on_push = false
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "tagged_images_policy" {
+  repository = aws_ecr_repository.ecr_repo.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 10 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "untagged_images_policy" {
+  repository = aws_ecr_repository.ecr_repo.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 10 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
